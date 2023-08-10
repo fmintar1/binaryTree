bb49da6f1de48381cd8f9654a96bcb1c8c239910
fmintar1 <fmintar1@gmail.com>
2023-08-10 19:13:47 -0400

    created Node

--- src/main/java/com/example/Node.java
+++ src/main/java/com/example/Node.java
@@ -1,5 +1,20 @@
 package com.example;
 
 public class Node {
-    
+
+    int key;
+    String name;
+
+    Node leftChild;
+    Node rightChild;
+
+    Node(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    public String toString() {
+        return (name + " has a key " + key);
+    }
+
 }
